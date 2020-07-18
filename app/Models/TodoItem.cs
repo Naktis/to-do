94cf2ebef94ccc759c2e07ee61887c97f6caf313
Augustina <37456430+Naktis@users.noreply.github.com>
2020-07-18 23:35:01 +0300

    Create to do items

---
+++ app/Models/TodoItem.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis.Differencing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.Models
+{
+    public class TodoItem
+    {
+        public TodoItem()   // Automatic ID incremention and assignment
+        {
+            ID = maxID;
+            maxID++;
+        }
+
+        public TodoItem Copy(TodoItem newData)  // Copy assignment without changing ID for editing
+        {
+            Name = newData.Name;
+            Description = newData.Description;
+            priority = newData.Priority;
+            return this;
+        }
+
+        private static int maxID = 0;
+        public int ID { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        private int priority = 3;
+        public int Priority
+        {
+            get 
+            { 
+                return priority; 
+            }
+            set
+            {
+                if (value >= 1 && value <= 5)
+                    priority = value;
+            }
+        }
+    }
+}
---
+++ app/Services/InMemoryTodoItemProvider.cs
@@ -0,0 +1,48 @@
+﻿using app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.Services
+{
+    public class InMemoryTodoItemProvider : ITodoItemProvider
+    {
+        private List<TodoItem> data = new List<TodoItem>()
+        {
+            // Sample data
+            new TodoItem(){Name="Rename Elon Musk's child", Description="Do not use numbers"},
+            new TodoItem(){Name="Who lives in a pineapple under the sea",Description="Spongebob Squarepants", Priority=2},
+            new TodoItem(){Name="Eat lunch", Description="Try microwaving ants", Priority=1}
+        };
+
+        public List<TodoItem> GetAll()
+        {
+            return data;
+        }
+
+        public TodoItem Get(int id)
+        {
+            return data[id];
+        }
+
+        public void Add(TodoItem todoItem)
+        {
+            data.Add(todoItem);
+        }
+
+        public void Edit(int id, TodoItem todoItem)
+        {
+            // Get an index where the item with a specific ID is stored
+            int index = data.FindIndex(item => item.ID == id);
+            // Replace its contents
+            data[index] = data[index].Copy(todoItem);
+        }
+
+        public void Delete(int id)
+        {
+            // Search for an item with specific ID and remove it
+            data.RemoveAt(data.FindIndex(item => item.ID == id));
+        }
+    }
+}
