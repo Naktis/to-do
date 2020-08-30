1b77c6270d6b67597498a0f8c2c69124a98a1ee5
Augustina <37456430+Naktis@users.noreply.github.com>
2020-08-31 00:41:21 +0300

    Create API client, controllers and views

---
+++ Todo.Web/Controllers/ClientApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo.ApiClient;
+
+namespace Todo.Web.Controllers
+{
+    public class ClientApiController : Controller
+    {
+        private readonly ClientsClass clientApi;
+
+        public ClientApiController(ClientsClass clientApi)
+        {
+            this.clientApi = clientApi;
+        }
+        // GET: ClientApi
+        public async Task<IActionResult> Index()
+        {
+            return View(await clientApi.ApiClientsGetAsync());
+        }
+    }
+}
